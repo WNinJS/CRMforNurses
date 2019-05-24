ece8b94a369f62693db3b8af2f926673058757ff
beardworf <beardworf@gmail.com>
2019-05-24 14:45:44 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100 w-100">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- My Style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title> Главная </title>
+</head>
+<body>
+    <!-- ШАПКА -->
+    <nav class="navbar navbar-expand-md d-flex justify-content-center w-100" style="height: 200px; background: #F1FFE8;">
+        <ul class="d-flex justify-content-between text-center w-100">
+            <li class="d-flex flex-column justify-content-center"><a class="" href="dataOfNurses.php"> ДАННЫЕ О СИДЕЛКАХ </a></li>
+            <li class="d-flex flex-column justify-content-center"><a class="" href=""> ДАННЫЕ О ДОГОВОРАХ </a></li>
+            <li class="d-flex flex-column justify-content-center"><a class="" href=""> ИНСТРУМЕНТ ПОДБОРА СИДЕЛОК </a></li>
+            <li class="d-flex flex-column justify-content-center"><a class="" href=""> АНАЛИТИКА </a></li>
+            <li class="d-flex flex-column justify-content-center"><a class="" href=""> ДАННЫЕ О МЕНЕДЖЕРАХ </a></li>
+        </ul>
+    </nav>
+
+    <br>
+    <br>
+    <div class="container-fluid d-flex justify-content-center h-100 mx-0">
+        <div class="row h-100 w-100">
+            <!-- Секция контента -->
+            <div class="col-12 content h-100 text-center">
+                <h2 class="mainText"> Добро пожаловать! </h2>
+                <h2 class="mainText"> Для начала работы выберите нужный раздел в шапке сайта. </h2>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
