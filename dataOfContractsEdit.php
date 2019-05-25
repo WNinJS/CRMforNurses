db852b94c6dae5cba92335a0c7abd5af3054ffac
beardworf <beardworf@gmail.com>
2019-05-25 20:24:44 +0300

    Create dataOfContractsEdit.php

---
+++ dataOfContractsEdit.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100 w-100">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- My Style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title> Редактирование данных о договорах </title>
+</head>
+<body>
+    <!-- ШАПКА -->
+    <nav class="navbar navbar-expand-md d-flex justify-content-center w-100" style="height: 200px; background: #F1FFE8;">
+        <ul class="d-flex justify-content-between text-center w-100">
+            <li class="d-flex flex-column justify-content-center"><a class="" href="dataOfNurses.php"> ДАННЫЕ О СИДЕЛКАХ </a></li>
+            <li class="d-flex flex-column justify-content-center"><a class="active-color" href="dataOfContracts.php"> ДАННЫЕ О ДОГОВОРАХ </a></li>
+            <li class="d-flex flex-column justify-content-center"><a class="" href="selectToolNurses.php"> ИНСТРУМЕНТ ПОДБОРА СИДЕЛОК </a></li>
+            <li class="d-flex flex-column justify-content-center"><a class="" href="analytics.php"> АНАЛИТИКА </a></li>
+            <li class="d-flex flex-column justify-content-center"><a class="" href="dataOfManagers.php"> ДАННЫЕ О МЕНЕДЖЕРАХ </a></li>
+        </ul>
+    </nav>
+
+    <br>
+    <br>
+    <div class="container-fluid d-flex justify-content-center h-100 mx-0">
+        <div class="row h-100 w-100">
+            <!-- Кнопки и их описание -->
+            <div class="col-12 h-100 text-center">
+                <h2 class="titleText"> РЕДАКТИРОВАНИЕ ДАННЫХ О ДОГОВОРАХ </h2>
+                
+                <br>
+                <hr class="hr-white">
+                <form class="find" method="POST d-flex justify-content-around">
+                    <input type="text" class="find-input" placeholder="Введите номер договора для поиска конкретной информации">
+                    <button class="btn find-btn"> <img src="/images/findIcon.png" alt="find" style="width: 31px; heigth: 31px;"> </button>
+                </form>
+                <hr class="hr-white">
+                <br>
+
+                <p class="font-title"> ДОГОВОР </p>
+                <div class="d-flex flex-column align-items-center">
+                    <a class="name-of-nurses" href=""> ДОГОВОР №1 </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
