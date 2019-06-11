be3e1664e6ccc1a2acc0bed0b0a9e399782bd01c
beardworf <beardworf@gmail.com>
2019-06-12 00:05:25 +0300

    Create dataOfContractsViewWhich.php

---
+++ dataOfContractsViewWhich.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100 w-100">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- My Style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title> Просмотр данных договора </title>
+</head>
+<body>
+    <!-- ШАПКА -->
+    <nav class="navbar navbar-expand-lg navbar w-100" style="background: white;">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">&#9776;</span>
+        </button>
+
+        <div class="collapse navbar-collapse w-100 " id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto d-flex justify-content-center align-items-center w-100 p-0 m-0">
+                <li class="navbar-item-active text-center"> <a class="nav-link" href="dataOfNurses.php"> СИДЕЛКИ </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> ДОГОВОРЫ </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> ИНСТРУМЕНТ ПОДБОРА СИДЕЛОК </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> АНАЛИТИКА </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> МЕНЕДЖЕРЫ </a></li>
+                <li class="navbar-item-active d-flex justify-content-center"> 
+                    <p class="d-flex align-items-center m-0"> Имя </p>
+                    <a class="nav-link" href="login.php"> 
+                        <img src="images/logout.png" alt="logout" style="width: 25px; heigth: 25px; margin-left: 5px;"> 
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <br>
+    <br>
+    <div class="container-fluid d-flex justify-content-center h-100 mx-0">
+        <div class="row h-100 w-100">
+            <!-- Кнопки и их описание -->
+            <div class="col-12 h-100 text-center">
+                <h2 class="titleText"> ДОГОВОР №1 </h2>
+                <br>
+                <hr class="hr-white">
+
+                    <table class="table table-hover">
+                        <tbody>
+                            <tr>
+                                <th scope="row th-width">НОМЕР ДОГОВОРА</th>
+                                <td>1</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ФИО ЗАКАЗЧИКА</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ПАСПОРТНЫЕ ДАННЫЕ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">СТЕПЕНЬ РОДСТВА М/У ЗАКАЗЧИКОМ И ПОДОП.</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ТЕЛЕФОН</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ДОП. ДАННЫЕ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ФИО ПОДОПЕЧНОГО</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ВОЗРАСТ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ЗАБОЛЕВАНИЕ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ОСОБЕННОСТИ ПОДОП.</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">АДРЕС РАБОТЫ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">СРОК ДЕЙСТВИЯ ДОГОВОРА</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ГРАФИК РАБОТЫ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ОБЩАЯ СУММА ДОГОВОРА</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ДОЛЯ ОФИСА</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">СПОСОБ ПОСТУПЛЕНИЯ ДЕНЕГ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ОБЩИЙ ТАРИФ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ТАРИФ СДЕЛКИ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ПРЕМИЯ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">КОЛ-ВО ПРЕМИАЛЬНЫХ ДНЕЙ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">СРЕДНИЙ ПРОЦЕНТ ПО ДОГОВОРУ</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">МЕНЕДЖЕР</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">СИДЕЛКА</th>
+                                <td>Одна или несколько в зависимости от заполненных данных
+                                    Данные вытащить из базы данных</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row th-width">ОПЛАЧЕНО</th>
+                                <td>Данные вытащить из базы данных</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                <hr class="hr-white">
+                <br>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
