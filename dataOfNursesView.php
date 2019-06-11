98126e710c40fed2160049436f9c6df54b41e225
beardworf <beardworf@gmail.com>
2019-06-12 00:06:22 +0300

    Update dataOfNursesView.php

--- dataOfNursesView.php
+++ dataOfNursesView.php
@@ -15,18 +15,26 @@
 </head>
 <body>
     <!-- ШАПКА -->
-    <nav class="navbar navbar-expand-md d-flex justify-content-center w-100" style="height: 200px; background: #F1FFE8;">
-        <ul class="d-flex justify-content-between text-center w-100">
-            <li class="d-flex flex-column justify-content-center"><a class="active-color" href="dataOfNurses.php"> ДАННЫЕ О СИДЕЛКАХ </a></li>
-            <li class="d-flex flex-column justify-content-center"><a class="" href="dataOfContracts.php"> ДАННЫЕ О ДОГОВОРАХ </a></li>
-            <li class="d-flex flex-column justify-content-center"><a class="" href="selectToolNurses.php"> ИНСТРУМЕНТ ПОДБОРА СИДЕЛОК </a></li>
-            <li class="d-flex flex-column justify-content-center"><a class="" href="analytics.php"> АНАЛИТИКА </a></li>
-            <li class="d-flex flex-column justify-content-center"><a class="" href="dataOfManagers.php"> ДАННЫЕ О МЕНЕДЖЕРАХ </a></li>
-            <form class="d-flex align-items-center justify-content-center" style="padding-left: 20px;" method="get">
-                <p class="p-0 m-0"> Имя </p>
-                <a href="login.php"> <img src="images/logout.png" alt="logout" style="width: 25px; heigth: 25px; margin-left: 5px;"> </a>
-            </form>
-        </ul>
+    <nav class="navbar navbar-expand-lg navbar w-100" style="background: white;">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">&#9776;</span>
+        </button>
+
+        <div class="collapse navbar-collapse w-100 " id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto d-flex justify-content-center align-items-center w-100 p-0 m-0">
+                <li class="navbar-item-active text-center"> <a class="nav-link" href="dataOfNurses.php"> СИДЕЛКИ </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> ДОГОВОРЫ </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> ИНСТРУМЕНТ ПОДБОРА СИДЕЛОК </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> АНАЛИТИКА </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> МЕНЕДЖЕРЫ </a></li>
+                <li class="navbar-item-active d-flex justify-content-center"> 
+                    <p class="d-flex align-items-center m-0"> Имя </p>
+                    <a class="nav-link" href="login.php"> 
+                        <img src="images/logout.png" alt="logout" style="width: 25px; heigth: 25px; margin-left: 5px;"> 
+                    </a>
+                </li>
+            </ul>
+        </div>
     </nav>
 
     <br>
@@ -40,7 +48,7 @@
                 <br>
                 <hr class="hr-white">
                 <form class="find" method="POST d-flex justify-content-around">
-                    <input type="text" class="find-input" placeholder="Введите имя сиделки для поиска конкретной информации">
+                    <input type="text" class="find-input" placeholder="Введите ФИО или возраст или телефон или национальность и т.п.">
                     <button class="btn find-btn"> <img src="/images/findIcon.png" alt="find" style="width: 31px; heigth: 31px;"> </button>
                 </form>
                 <hr class="hr-white">
@@ -57,139 +65,7 @@
         </div>
     </div>
 </div>
-
-
-<!-- Модальное окно для просмотра данных о сиделке -->
-<div class="modal fade" id="viewNurseModal" tabindex="-1" role="dialog" aria-labelledby="viewNurseModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header text-center">
-                <h5 class="modal-title" id="viewNurseModalLabel">ПРОСМОТР ДАННЫХ О СИДЕЛКЕ</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-
-            <!-- Андрей, здесь должны данные выводится с БД -->
-            <div class="modal-body d-flex flex-column">
-
-            <table class="table table-hover">
-                <tbody>
-                    <tr>
-                        <th scope="row th-width">ФИО</th>
-                        <td>Петрова Наталья Владимировна</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">ВОЗРАСТ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">ТЕЛЕФОН</th>
-                        <td>Данные вытащить из базы данных
-                            Так же доп. телефоны, если есть.
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">НАЦИОНАЛЬНОСТЬ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">АДРЕС ПРОЖИВАНИЯ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">ПАСПОРТНЫЕ ДАННЫЕ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">АДРЕС ПРОПИСКИ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">ДОП. КОНТАКТНЫЕ ДАННЫЕ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">ОПЫТ РАБОТЫ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">С КАКИМИ ЗАБОЛЕВАНИЯМИ РАБОТАЛ(А)</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">НАВЫКИ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">СТАТУС СИДЕЛКИ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">ЗНАНИЕ ЯЗЫКА</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">ВЕРОИСПОВЕДАНИЕ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">НАЛИЧИЕ СПРАВОК О НЕСУДИМОСТИ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">НАЛИЧИЕ САН. КНИЖКИ ИЛИ МЕД. СПРАВОК</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">НАЧИЛИЕ МЕД. ОБРАЗОВАНИЯ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">БАНКОВСКАЯ КАРТА</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">ИНФОРМАЦИЯ О ПРОХОЖДЕНИИ КУРСОВ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">ВОЗМОЖНОСТЬ РАБОТЫ С ДЕТЬМИ</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row th-width">ОТЗЫВ МЕНЕДЖЕРА</th>
-                        <td>Данные вытащить из базы данных</td>
-                    </tr>
-                </tbody>
-            </table>
                 
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-primary-modal" data-dismiss="modal" aria-label="Close"> ЗАКРЫТЬ </button>
-            </div>
-        </div>
-    </div>
-</div>
 
 
 
