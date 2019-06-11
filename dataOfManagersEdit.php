8b7fa0aff2fc691ce32b03266277ac996c7835a6
beardworf <beardworf@gmail.com>
2019-06-12 00:05:35 +0300

    Update dataOfManagersEdit.php

--- dataOfManagersEdit.php
+++ dataOfManagersEdit.php
@@ -11,22 +11,30 @@
     <!-- My Style CSS -->
     <link rel="stylesheet" href="css/style.css">
 
-    <title> Редактирование данных о менеджерах </title>
+    <title> Редактирование данных менеджеров </title>
 </head>
 <body>
     <!-- ШАПКА -->
-    <nav class="navbar navbar-expand-md d-flex justify-content-center w-100" style="height: 200px; background: #F1FFE8;">
-        <ul class="d-flex justify-content-between text-center w-100">
-            <li class="d-flex flex-column justify-content-center"><a class="" href="dataOfNurses.php"> ДАННЫЕ О СИДЕЛКАХ </a></li>
-            <li class="d-flex flex-column justify-content-center"><a class="" href="dataOfContracts.php"> ДАННЫЕ О ДОГОВОРАХ </a></li>
-            <li class="d-flex flex-column justify-content-center"><a class="" href="selectToolNurses.php"> ИНСТРУМЕНТ ПОДБОРА СИДЕЛОК </a></li>
-            <li class="d-flex flex-column justify-content-center"><a class="" href="analytics.php"> АНАЛИТИКА </a></li>
-            <li class="d-flex flex-column justify-content-center"><a class="active-color" href="dataOfManagers.php"> ДАННЫЕ О МЕНЕДЖЕРАХ </a></li>
-            <form class="d-flex align-items-center justify-content-center" style="padding-left: 20px;" method="get">
-                <p class="p-0 m-0"> Имя </p>
-                <a href="login.php"> <img src="images/logout.png" alt="logout" style="width: 25px; heigth: 25px; margin-left: 5px;"> </a>
-            </form>
-        </ul>
+    <nav class="navbar navbar-expand-lg navbar w-100" style="background: white;">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">&#9776;</span>
+        </button>
+
+        <div class="collapse navbar-collapse w-100 " id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto d-flex justify-content-center align-items-center w-100 p-0 m-0">
+                <li class="navbar-item-active text-center"> <a class="nav-link" href="dataOfNurses.php"> СИДЕЛКИ </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> ДОГОВОРЫ </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> ИНСТРУМЕНТ ПОДБОРА СИДЕЛОК </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> АНАЛИТИКА </a></li>
+                <li class="navbar-item-active text-center"> <a class="nav-link" href=""> МЕНЕДЖЕРЫ </a></li>
+                <li class="navbar-item-active d-flex justify-content-center"> 
+                    <p class="d-flex align-items-center m-0"> Имя </p>
+                    <a class="nav-link" href="login.php"> 
+                        <img src="images/logout.png" alt="logout" style="width: 25px; heigth: 25px; margin-left: 5px;"> 
+                    </a>
+                </li>
+            </ul>
+        </div>
     </nav>
 
     <br>
@@ -50,80 +58,13 @@
 
                 <p class="font-title"> ФИО МЕНЕДЖЕРА </p>
                 <div class="d-flex flex-column align-items-center">
-                    <a class="name-of-nurses" data-toggle="modal" href="#editManagerModal"> ЛОГИНОВ ИГОРЬ СЕРГЕИЧ </a>
+                    <a class="name-of-nurses" href="dataOfManagersEditWhich.php"> ЛОГИНОВ ИГОРЬ СЕРГЕИЧ </a>
                 </div>
 
             </div>
         </div>
     </div>
 </div>
-
-<!-- Модальное окно для редактирования данных о выбранном менеджере -->
-<div class="modal fade" id="editManagerModal" tabindex="-1" role="dialog" aria-labelledby="editManagerModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header text-center">
-                <h5 class="modal-title" id="editManagerModalLabel">РЕДАКТИРОВАНИЕ ДАННЫХ О МЕНЕДЖЕРЕ</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-
-            <!-- Андрей, отсюда данные должны отправляться в БД -->
-            <!-- Здесь поля, в которых редактируются данные о менеджере-->
-            <!-- Информация в них должна содержаться сразу при открытии этой модалки для удобства бабулек -->
-            <div class="modal-body d-flex flex-column">
-
-                <form method="POST">
-                    <div class="modalDivInputs d-flex flex-row justify-content-around">
-                        <p class="textInputModal"> ФИО </p> 
-                        <input class="inputModal" type="text" required>
-                    </div>
-
-                    <div class="modalDivInputs d-flex flex-row justify-content-around">
-                        <p class="textInputModal"> КОНТАКТЫ </p> 
-                        <input class="inputModal" type="text" required>
-                    </div>
-
-                    <div class="modalDivInputs d-flex flex-row justify-content-around">
-                        <p class="textInputModal"> ГРАФИК РАБОТЫ </p> 
-                        <input class="inputModal" type="text" required>
-                    </div>
-
-                    <div class="modalDivInputs d-flex flex-row justify-content-around">
-                        <p class="textInputModal"> ОКЛАД </p> 
-                        <input class="inputModal" type="text" list="<listOfNational>" required>
-                    </div>
-
-                    <div class="modalDivInputs d-flex flex-row justify-content-around">
-                        <p class="textInputModal"> ПРОЦЕНТ </p> 
-                        <input class="inputModal" type="text" required>
-                    </div>
-
-                    <div class="modalDivInputs d-flex flex-row justify-content-around">
-                        <p class="textInputModal"> ФОРМА ДОПУСКА </p> 
-                        <input class="inputModal" type="text" list="<listOfFormAccess>" required>
-                        <datalist id="<listOfFormAccess>">
-                            <option value="Менеджер">
-                        </datalist>
-                    </div>
-
-                    <div class="modalDivInputs d-flex flex-row justify-content-around">
-                        <p class="textInputModal"> ПАРОЛЬ </p> 
-                        <input class="inputModal" type="text" required>
-                    </div>
-
-                </div>
-                
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary-modal"> РЕДКАТИРОВАТЬ </button>
-                </div>
-            </form>    
-        </div>
-    </div>
-</div>
-
-
 
     <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
